c903a2e79673c2b7dfe8ecbbb9d3facc2582fa99
Gerrit Stricker <gj.stricker@t-online.de>
2026-03-14 17:21:24 +0100

    add file move to main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode
 def main():
-    some_node = TextNode("text hier einfügen", "link", "https://www.boot.dev")
-    print(some_node)
-                         
+    filecopy("./static", "./public")
+
+def filecopy(source,destination):
+    if not os.path.isdir(source) or not os.path.isdir(destination):
+        raise ValueError("Directory not found")
+
+    # clean up target directory
+
+    shutil.rmtree(destination)
+    os.mkdir(destination)
+
+    for entity in os.listdir(source):
+        current = os.path.join(source, entity)
+        mirror = os.path.join(destination, entity)
+        if os.path.isfile(current):
+            shutil.copy(current, mirror)
+            print(f"Log: CP File {current} to dest {mirror}")
+        elif os.path.isdir(current):
+            os.mkdir(mirror)
+            print(f"Log: made dir {mirror} will recurse over directory")
+            filecopy(current, mirror)
+
 
 
 
