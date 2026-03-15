d8e474ca38c68e71c270d9906366245f488b2788
Gerrit Stricker <gj.stricker@t-online.de>
2026-03-15 15:28:12 +0100

    modify for GH hosting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
+import sys
 import shutil
 from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode
 from parsemarkdown import markdown_to_html_node, extract_title
 def main():
-    filecopy("./static", "./public")
-    generate_pages_recursive("content", "template.html" , "public")
+    if len(sys.argv)>=2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    filecopy("./static", "./docs")
+    generate_pages_recursive("content", "template.html" , "docs", basepath)
     
 def filecopy(source,destination):
     if not os.path.isdir(source) or not os.path.isdir(destination):
@@ -27,7 +32,7 @@ def filecopy(source,destination):
             print(f"Log: made dir {mirror} will recurse over directory")
             filecopy(current, mirror)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as inp_file:
         raw_md = inp_file.read()
@@ -37,21 +42,22 @@ def generate_page(from_path, template_path, dest_path):
     html_text = htmlnodes.to_html()
     title = LeafNode("h1", extract_title(raw_md)).to_html()
     html_site = template.replace("{{ Title }}", title).replace("{{ Content }}", html_text)
+    html_site = html_site.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     if not os.path.isdir(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as htmlfile:
         htmlfile.write(html_site)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entity in os.listdir(dir_path_content):
         subj = os.path.join(dir_path_content,entity)
         if os.path.isfile(subj):
             mirror = os.path.join(dest_dir_path, f"{entity[:-3]}.html")
-            generate_page(subj, template_path, mirror)
+            generate_page(subj, template_path, mirror, basepath)
         if os.path.isdir(subj):
             mirror = os.path.join(dest_dir_path, entity)
             os.mkdir(mirror)
-            generate_pages_recursive(subj, template_path, mirror)
+            generate_pages_recursive(subj, template_path, mirror, basepath)
 
 
 main()
